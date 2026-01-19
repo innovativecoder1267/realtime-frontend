3b820db30edd50fe181aaeea2602af9506b6cb6f
innovativecoder1267 <aaravchaprana1627@gmail.com>
2026-01-19 21:33:02 +0530

    Delete src/app/landing directory

--- src/app/landing/page.jsx
+++
@@ -1,9 +0,0 @@
-
-// // pages/index.js ya kisi parent page me
-// import dynamic from "next/dynamic";
-
-// const Landing = dynamic(() => import('../components/landingpage'), { ssr: false });
-import Landing from "../components/landingpage";
-export default function Page() {
-  return <Landing />;
-}
