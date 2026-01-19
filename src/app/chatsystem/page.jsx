d3cb03f5f9fc3b8c53b7335b980543fd860acb95
innovativecoder1267 <aaravchaprana1627@gmail.com>
2026-01-19 22:07:03 +0530

    Update page.jsx

--- src/app/chatsystem/page.jsx
+++ src/app/chatsystem/page.jsx
@@ -1,6 +1,10 @@
+import { Suspense } from "react";
 import ChatSystem from "../components/chatsystem";
+
 export default function Page() {
-    return(
- <ChatSystem/>  
-    )
+  return (
+    <Suspense fallback={<div>Loading chat...</div>}>
+      <ChatSystem />
+    </Suspense>
+  );
 }
